fca26383a5ab9c908850cffd35e95b85e8648df3
Benedikt Schmautz <benedikt.schmautz@online.de>
2019-11-16 01:04:10 +0100

    implemented logging; errorhandling; basic structure;

--- CH3mculator/App.xaml.cs
+++ CH3mculator/App.xaml.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using CH3mculator.Shell;
+using CH3mculator.Shell.ErrorHandling;
+using System;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace CH3mculator
 {
@@ -13,5 +11,15 @@ namespace CH3mculator
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            Current.DispatcherUnhandledException += new DispatcherUnhandledExceptionEventHandler(HandleUnhandledException);
+        }
+
+        private void HandleUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            Current.MainWindow.Content = new ErrorView(new ErrorViewModel(e.Exception));
+            e.Handled = true;
+        }
     }
 }
--- CH3mculator/MainWindow.xaml.cs
+++ CH3mculator/MainWindow.xaml.cs
@@ -1,28 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using MahApps.Metro.Controls;
+using System.Reflection;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace CH3mculator
 {
     /// <summary>
     /// Interaktionslogik für MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+            SetTitleInformation();
+        }
+
+        private void SetTitleInformation()
+        {
+            var assemblyName = Assembly.GetEntryAssembly().GetName();
+            TitleCharacterCasing = CharacterCasing.Normal;
+            Title = string.Concat("CH3mculator - ", assemblyName.Version.ToString());
         }
     }
 }
